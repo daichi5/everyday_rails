9b3612d28e227c3c434ca0456fcbcf8698d80488
daichi <sw41dc@gmail.com>
2019-04-19 12:17:59 +0900

    Add Model Spec with FactoryBot

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  describe "late status" do
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :project_due_yesterday)
+      expect(project).to be_late
+    end
+
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project, :project_due_today)
+      expect(project).to_not be_late
+    end
+
+    it "is on time when the due date is in the future" do
+      project = FactoryBot.create(:project, :project_due_tomorrow)
+      expect(project).to_not be_late
+    end
+  end
+
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+
   it "does not allow duplicate project names per user" do
     user = User.create(
       first_name: "Joe",
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "has a valid factroy" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
   # 姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name, email, and password" do
     user = User.new(
@@ -13,46 +17,32 @@ RSpec.describe User, type: :model do
   end
   # 名がなければ無効な状態であること
   it "is invalid without a first name" do
-    user = User.new(first_name: nil)
+    user = FactoryBot.build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include("can't be blank")
   end
   # 姓がなければ無効な状態であること
   it "is invalid without a last name" do
-    user = User.new(last_name: nil)
+    user = FactoryBot.build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include("can't be blank") 
   end
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
-    user = User.new(email: nil)
+    user = FactoryBot.build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include("can't be blank")
   end
   # 重複したメールアドレスなら無効な状態であること
   it "is invalid with a duplicate email address" do
-    User.create(
-      first_name: "tanaka",
-      last_name: "tarou",
-      email: "test@example.com",
-      password: "doot-rtsrt-gdfd"
-    )
-    user = User.new(
-      first_name: "suzuki",
-      last_name: "ichirou",
-      email: "test@example.com",
-      password: "doot-rtsrt-gdfd"
-    )
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
   # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a stringi" do
-    user = User.new(
-      first_name: "John",
-      last_name: "Doe",
-      email: "johndoe@example.com",
-    )
+    user = FactoryBot.build(:user, first_name: "John", last_name: "Doe")
     expect(user.name).to eq "John Doe"
   end
 
